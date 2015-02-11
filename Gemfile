983964ab1d0ad95231842b37707534d5a7395c6d
Dan Buch <daniel.buch@gmail.com>
2015-02-11 12:23:39 -0500

    Fill in more implementation

--- Gemfile
+++ Gemfile
@@ -2,6 +2,8 @@ source 'https://rubygems.org'
 
 gem 'pry', group: %i(test development)
 gem 'puma'
+gem 'rake', group: %i(test development)
 gem 'redis'
 gem 'rerun', group: %i(test development)
+gem 'rspec', group: %i(test development)
 gem 'sinatra'
--- lib/chirp_tracker.rb
+++ lib/chirp_tracker.rb
@@ -1,6 +1,7 @@
 $LOAD_PATH.unshift(File.expand_path('../lib', __FILE__))
 
 require 'json'
+require 'openssl'
 require 'redis'
 require 'sinatra/base'
 
@@ -12,11 +13,10 @@ L2metLog.default_log_level = ENV['DEBUG'] ? :debug : :info
 class ChirpTracker < Sinatra::Base
   include L2metLog
 
-  set :db do
-    Redis.new(url: ENV[ENV['REDIS_PROVIDER'] || 'REDIS_URL'] || 'redis://localhost:6379/0')
-  end
-
+  set :db, Redis.new(url: ENV[ENV['REDIS_PROVIDER'] || 'REDIS_URL'] || 'redis://localhost:6379/0')
+  set :ttl, Integer(ENV.fetch('PAYLOAD_TTL', '3600'))
   set :secret_token, "#{ENV['SECRET_TOKEN']}"
+  set :auths, "#{ENV['TRAVIS_AUTHS']}".split(':').map { |auth| "token #{auth.strip}" }
 
   helpers do
     def respond_json(whatever)
@@ -24,34 +24,70 @@ class ChirpTracker < Sinatra::Base
     end
 
     def verify_hub_signature!(request)
+      hub_sig = request.env.fetch('HTTP_X_HUB_SIGNATURE')
       request.body.rewind
       payload_body = request.body.read
       signature = 'sha1=' + OpenSSL::HMAC.hexdigest(OpenSSL::Digest.new('sha1'), settings.secret_token, payload_body)
-      return halt 500, "Signatures didn't match!" unless Rack::Utils.secure_compare(signature, request.env['HTTP_X_HUB_SIGNATURE'])
+      return halt 500, "Signatures didn't match!" unless Rack::Utils.secure_compare(signature, hub_sig)
     end
   end
 
   configure do
     extend L2metLog
     $stdout.sync = true
-
-    log message: 'starting up'
   end
 
   post '/github' do
-    verify_hub_signature!(request)
+    unless settings.development?
+      halt 400, 'No github signature' unless request.env.key?('HTTP_X_HUB_SIGNATURE')
+      verify_hub_signature!(request)
+    end
 
     log message: 'received something from github', level: :debug, params: params
+
     body = JSON.parse(params[:payload])
+    head_commit = body.fetch('head_commit').fetch('id')
+    settings.db.setex("github:payload:#{head_commit}", settings.ttl, params[:payload])
+    settings.db.setex("github:timestamp:#{head_commit}", settings.ttl, Time.now.utc.to_i)
+
     status 200
     respond_json ok: :great
   end
 
   post '/travis' do
     log message: 'received something from travis', level: :debug, params: params
-    halt 401 unless settings.auths.include?(request.env['HTTP_AUTHORIZATION'])
+
+    unless settings.development?
+      halt 401 unless settings.auths.include?(request.env['HTTP_AUTHORIZATION'])
+    end
+
+    body = JSON.parse(params[:payload])
+    head_commit = body.fetch('commit')
+    settings.db.setex("travis:payload:#{head_commit}", settings.ttl, params[:payload])
+    settings.db.setex("travis:timestamp:#{head_commit}", settings.ttl, Time.now.utc.to_i)
+
     status 200
     respond_json ok: :great
+  end
+
+  get '/chirps' do
+    chirps = settings.db.keys('github:timestamp:*').map do |key|
+      commit = key.split(':').last
+      github_timestamp = settings.db.get(key) || 0.0
+      travis_timestamp = settings.db.get("travis:timestamp:#{commit}") || 0.0
+      {
+        commit: commit,
+        delta: Float(travis_timestamp) - Float(github_timestamp)
+      }
+    end
+
+    status 200
+    respond_json chirps: chirps
+  end
+
+  def run!
+    log message: 'starting up'
+    super
   end
 
   run! if __FILE__ == $0
