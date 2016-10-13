9371cb36afe8d948f5b11f19fca1bc534d5893d5
Dan Buch <daniel.buch@gmail.com>
2016-10-13 13:27:23 -0400

    Support proxying stats to l2met log records

---
+++ spec/chirp_tracker_spec.rb
@@ -0,0 +1,36 @@
+# frozen_string_literal: true
+describe 'Chirp Tracker' do
+  def app
+    ChirpTracker
+  end
+
+  context 'POST /stats' do
+    it 'forwards stats to logs' do
+      expect_any_instance_of(app).to receive(:log).with(
+        'measure#chirp.unknown.test' => '1.5s'
+      )
+      post '/stats', '{"data":[{"script":"test","exe_time":"1.5s"}]}'
+      expect(last_response.status).to eq(200)
+    end
+
+    it 'accepts empty data' do
+      post '/stats', '{"data":[]}'
+      expect(last_response.status).to eq(200)
+    end
+
+    it 'rejects invalid JSON' do
+      post '/stats', '{nope'
+      expect(last_response.status).to eq(400)
+    end
+
+    it 'rejects payloads lacking "data"' do
+      post '/stats', '{"nope":""}'
+      expect(last_response.status).to eq(400)
+    end
+
+    it 'rejects payloads with invalid record format' do
+      post '/stats', '{"data":[{"nope":1}]}'
+      expect(last_response.status).to eq(400)
+    end
+  end
+end
--- spec/support.rb
+++ spec/support.rb
@@ -2,3 +2,8 @@
 require 'simplecov'
 
 require 'chirp_tracker'
+require 'rack/test'
+
+RSpec.configure do |conf|
+  conf.include Rack::Test::Methods
+end
